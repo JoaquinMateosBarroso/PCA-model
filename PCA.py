359b9187c5dcece11693999bbec6d8bdbc0be4e5
JoaquinMateosBarroso <108573823+JoaquinMateosBarroso@users.noreply.github.com>
2022-09-03 19:06:25 +0200

    Little changes

--- PCA.py
+++ PCA.py
@@ -1,21 +1,33 @@
 import pandas as pd
 import numpy as np
-class PCA():
-    matrix = np.array([[]])
-    
-    #The data must be normalized in order to train the object
-    def covariance(self, x : pd.DataFrame, y : pd.DataFrame):
-        covariance = 0
-        for i in range(x.size):
-            covariance += (x[i]-x.mean())*(y[i]-y.mean())
-        return covariance/(x.size-1)
-    def make_covariance_matrix(self, data : pd.DataFrame):        
+
+
+def covariance(x : pd.DataFrame, y : pd.DataFrame):
+    covariance = 0
+    for i in range(x.size):
+        covariance += (x[i]-x.mean())*(y[i]-y.mean())
+    return covariance/(x.size-1)    
+
+def make_covariance_matrix(data : pd.DataFrame):
+        matrix = np.array([[]])
         for i in range(data.columns.size):
             new_column = []
             for j in range(data.columns.size):
-                new_column.append(PCA.covariance(self, data.iloc[:, i], data.iloc[:, j]))
+                new_column.append(PCA.covariance(data.iloc[:, i], data.iloc[:, j]))
             if i == 0:
-                self.matrix = np.array([new_column])
+                return np.array([new_column])
             else:
-                self.matrix = np.append(self.matrix, [new_column], axis = 0)
-        return
+                return np.append(matrix, [new_column], axis = 0)
+
+def matrix_to_PC(matrix : np.array):
+    return 
+
+class PCA():
+    def __init__(self):
+        self.matrix = np.array([[]])
+        self.components = np.array([])
+    
+    #The data must be normalized in order to train the object  
+    def train(self):
+        self.matrix = make_covariance_matrix(self)
+    
